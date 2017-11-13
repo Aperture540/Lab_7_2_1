5ea8bee998236bf028597185bf466381ef2d68e3
Anthony Gendron <aperturescience540@gmail.com>
2017-11-13 14:56:31 -0500

    Lab_7_2_1

--- main.cpp
+++ main.cpp
@@ -11,15 +11,45 @@
  * Created on November 2, 2017, 1:22 PM
  */
 
-#include <cstdlib>
+#include <iostream>
 
 using namespace std;
+//add your own exception class here 
+//add functions code here
+float square_area(float a) {
+    if(a<0) {
+        throw string("Your input is not valid. The area can't be negative.");
+    }
+    else {
+        return a*a;
+    }
+}
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+float rectangle_area(float a, float b) {
+        if(a<0 || b<0) {
+        throw string("Your input is not valid. The area can't be negative.");
+    }
+    else {
+        return a*b;
+    }
+}
 
+int main(void) {
+    float a, b, r; 
+    cin >> a;
+    cin >> b;
+    try
+    {
+        float rsquare = square_area(a);
+        float rrectangle = rectangle_area(a,b);
+        cout << rsquare << endl << rrectangle << endl;
+    }
+    //add a suitable catch block here
+    catch(string &exc) {
+        cout<<exc<<endl;
+        return 0;
+    }
     return 0;
 }
+
 
